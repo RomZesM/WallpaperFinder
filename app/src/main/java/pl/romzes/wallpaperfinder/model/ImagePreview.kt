2a6212e57ab69bc90ec63d946cdf30be02c59826
Raman Martsulevich <dwarf.md@gmail.com>
2024-01-27 12:33:49 +0100

    refactor: remove deleted files from git

--- app/src/main/java/pl/romzes/wallpaperfinder/model/ImagePreview.kt
+++
@@ -1,7 +0,0 @@
-package pl.romzes.wallpaperfinder.model
-
-data class ImagePreview(
-    val imageId: Int,
-    val imageUrl : String,
-    val description : String
-)
--- app/src/main/java/pl/romzes/wallpaperfinder/utils/Connector.kt
+++
@@ -1,5 +0,0 @@
-package pl.romzes.wallpapers.utils
-
-interface Connector {
-    fun sendResult(number : Int, text : String)
-}
