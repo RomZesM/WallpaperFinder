1030b44702e69f9b77706c803b00c4195d55058a
Raman Martsulevich <dwarf.md@gmail.com>
2024-02-13 15:32:54 +0100

    feat: display favourite images in recycler view from db

--- presentation/src/main/java/pl/romzes/wallpaperfinder/fragments/favouriteFragment/FavouriteFragment.kt
+++ presentation/src/main/java/pl/romzes/wallpaperfinder/fragments/favouriteFragment/FavouriteFragment.kt
@@ -2,16 +2,22 @@ package pl.romzes.wallpaperfinder.fragments.favouriteFragment
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import pl.romzes.domain.model.ImagePreview
+import pl.romzes.wallpaperfinder.MainActivity
 import pl.romzes.wallpaperfinder.R
 import pl.romzes.wallpaperfinder.adapters.ImagePreviewRVAdapter
+import pl.romzes.wallpaperfinder.fragments.detailsFragment.DetailsFragment
+import pl.romzes.wallpaperfinder.utils.MyRecyclerViewOnClickListener
 
 class FavouriteFragment : Fragment() {
-
-
 
     companion object {
         fun newInstance() = FavouriteFragment()
@@ -32,6 +38,47 @@ class FavouriteFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FavouriteViewModel::class.java)
         // TODO: Use the ViewModel
+        viewModel.imagelist.observe(viewLifecycleOwner) {
+          initRecyclerView()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        context?.let { viewModel.getImagesFromDB(it) }
+    }
+    //todo make separaate function for both fragment as util - refactor
+    private fun initRecyclerView() {
+        //set myonclickFunction in adapter
+        rvAdapter.setMyOnclickListener(object : MyRecyclerViewOnClickListener {
+            //on click we open new fragment with parameters of images
+            //todo add some more parameters
+            override fun onClick(position: Int) {
+                (requireActivity() as MainActivity).displayFragment(
+                    DetailsFragment.newInstance(
+                    imageUrl = viewModel.imagelist.value?.get(position)?.imageUrl.toString(),
+                    imageDescription = viewModel.imagelist.value?.get(position)?.description.toString()
+
+                ))
+            }
+            override fun favOnClick(image : ImagePreview) {
+                //save fav image in db //todo can i get context, not context?
+                context?.let { viewModel.saveFavouriteImage(it, image ) }
+            }
+        })
+
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.recycler_view_id) //get the recycler view from fragment
+        //config recyclerView
+        recyclerView?.layoutManager = GridLayoutManager(context, 2) //set options for display elements on view
+        recyclerView?.adapter = rvAdapter //attach adapter, we can change different adapters for display info in RV
+        viewModel.imagelist.value?.let { rvAdapter.setImagePreviewIntoRecyclerView(it)
+        }
     }
 
 }
